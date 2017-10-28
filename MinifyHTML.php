e3305789a74cd83881e98a849f916c63a130e554
Kudashev Serge <kudashevs@gmail.com>
2017-10-29 01:08:04 +0300

    Plugin update
    Add posibility to exclude some pages by "exclude" options. Very helpfull for HTML Minifier no break XML output.

--- MinifyHTML.php
+++ MinifyHTML.php
@@ -1,5 +1,11 @@
 <?php
+$cid = $modx->resource->get('id');
+$exclude = $modx->getOption('exclude',$scriptProperties,'12');
+
+if(empty($cid)) { return; }
+if(in_array($cid,explode(',',$exclude))) { return; }
+
 $output = &$modx->resource->_output;
 $pattern = array('/\>[^\S ]+/s','/[^\S ]+\</s','/(\s)+/s');
 $replace = array('>','<','\\1');
-$output = preg_replace($pattern,$replace, $output);
+$output = preg_replace($pattern,$replace,$output);
